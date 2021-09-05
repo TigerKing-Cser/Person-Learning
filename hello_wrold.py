729b8ea73ea1edde2fa69bc21345907c6f1604fa
893632434@qq.com <felixhuwang@tencent.com>
2021-09-05 21:39:24 +0800

    提交代码

---
+++ hello_wrold.py
@@ -0,0 +1,11 @@
+# 第一个python代码
+
+#print('Hello Python world!')
+
+# 使用变量实现
+message = "Hello Python world!"
+print(message)
+
+# crash message
+message = "Hello Carsh Course Python world!"
+print(message)
---
+++ name.py
@@ -0,0 +1,7 @@
+name = "ada lovelace"
+# title首字母大写
+print(name.title())
+# upper全字母大写
+print(name.upper())
+# lower全字母大写
+print(name.lower())
